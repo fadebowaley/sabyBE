d98d995de8932b8281ad3d4f6093b1c28ef1ba0a
Ademola Adebowale <fadebowaley@gmail.com>
2025-11-18 06:23:37 +0100

    fix: Add missing controllers and routes for permReport and rollupReport

---
+++ src/controllers/rollupReport.controller.js
@@ -0,0 +1,49 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const catchAsync = require('../utils/catchAsync');
+const rollupReportService = require('../services/rollupReport.service');
+
+const buildFilters = (req) =>
+  pick(req.query, [
+    'tenant_id',
+    'project_id',
+    'node_id',
+    'start_date',
+    'end_date',
+    'limit',
+    'offset',
+  ]);
+
+const getDailyRollup = catchAsync(async (req, res) => {
+  const filters = buildFilters(req);
+  const data = await rollupReportService.getDailyRollup(filters);
+  res.status(httpStatus.OK).send({ success: true, ...data });
+});
+
+const getWeeklyRollup = catchAsync(async (req, res) => {
+  const filters = buildFilters(req);
+  const data = await rollupReportService.getWeeklyRollup(filters);
+  res.status(httpStatus.OK).send({ success: true, ...data });
+});
+
+const getMonthlyRollup = catchAsync(async (req, res) => {
+  const filters = buildFilters(req);
+  const data = await rollupReportService.getMonthlyRollup(filters);
+  res.status(httpStatus.OK).send({ success: true, ...data });
+});
+
+const getSubmissionStatus = catchAsync(async (req, res) => {
+  const filters = buildFilters(req);
+  if (!filters.tenant_id && req.user?.tenantId) {
+    filters.tenant_id = req.user.tenantId;
+  }
+  const status = await rollupReportService.getSubmissionStatus(filters);
+  res.status(httpStatus.OK).send({ success: true, status });
+});
+
+module.exports = {
+  getDailyRollup,
+  getWeeklyRollup,
+  getMonthlyRollup,
+  getSubmissionStatus,
+};
